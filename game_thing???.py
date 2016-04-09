026183c0f0fd4bb8d99727c86c15d21c4889dbf9
PK1312 <gdawg3@gmail.com>
2016-04-09 16:28:42 -0700

    Added some additional commands and functionality. Let me know any commands you try that don't work.

--- game_thing???.py
+++ game_thing???.py
@@ -1,3 +1,5 @@
+#todo- locked closet door. Maybe monster in closet. Item under bed.
+
 name = raw_input("Please enter your name: ")
 print "Hello, " + name + ". You find yourself in your bedroom. The morning sun streams in through your blinds. It's 7 AM, and you need to get ready for school. It's the first day of 4th grade and you can't be late."
 
@@ -27,7 +29,7 @@ def get_input(x):
         if player_location == "bed":
             print "You stretch your little arms as far as you can, but there's just too much lava between you and the door."
         elif player_location == "towel":
-            print "Which door are you trying to open?"
+            print "Which door are you trying to open, your CLOSET DOOR or the EXIT DOOR?"
     elif x.lower() == "open closet" or x.lower() == "open closet door" or x.lower() == "closet door" or x.lower() == "closet":
         player_clothing = "uniform"
         print "You've done it! You manage to get the closet door open, and grab your SCHOOL UNIFORM. It's difficult on your floating towel, but you manage to change into it, letting your pajamas burn up in the lava."
@@ -35,7 +37,7 @@ def get_input(x):
 
     elif x.lower() == "exit" or x.lower() == "open exit" or x.lower() == "open exit door" or x.lower() == "go exit" or x.lower() == "move exit" or x.lower() == "leave room" or x.lower() == "leave":
         if player_location == "towel" and player_clothing == "uniform":
-            print "You open the door. You see your mother putting breakfast on the table. You sigh, and the floor cools back to carpet. Time for school. Congrats! You won the game!"
+            print "You open the door. You see your mother putting breakfast on the table. You sigh, and the floor cools back to carpet. Time for school. Congrats, " + name + "! You won the game! For now..."
             game_complete = True
             return game_complete
         elif player_location == "towel" and player_clothing == "pajamas":
@@ -43,7 +45,26 @@ def get_input(x):
         elif player_location == "bed" and player_clothing == "pajamas":
             print "You have to get out of bed before you can leave."
         elif player_location == "bed" and player_clothing == "uniform":
-            print "wait what this shouldn't even be possible, what the heck did you do to break the game"
+            print "wait what, no, you shouldn't get back into bed after putting on your uniform what are you doing."
+    elif x.lower() == "help":
+        print "This is your standard test based adventure game! Type commands to see what works. If you're stuck, try using 'look'!"
+
+    elif x.lower() == "cry":
+        print "You begin to weep. You're scared of starting the fourth grade, which is why you've constructed these elaborate fantasy scenarios to cope. You lead a rich inner life. The tears sizzle as they hit the lava."
+
+    elif x.lower() == "get towel" or x.lower() == "grab towel" or x.lower() == "pick up towel" or x.lower() == "pick towel up" or x.lower() == "take towel":
+        print "You can't get the towel. It's too far away, and you'd have to stick your hand into lava to get it off the ground anyway."
+    elif x.lower() == "put on uniform" or x.lower() == "put uniform on" or x.lower() == "uniform":
+        if player_clothing == "pajamas":
+            print "Your UNIFORM is still hanging in your CLOSET."
+        elif player_clothing == "uniform":
+            print "You're already wearin your SCHOOL UNIFORM."
+
+    elif x.lower() == "the floor is not lava":
+        game_complete = True
+        print "Your childlike innocence dies. The floor is just carpet. You get ready for school, but the spark of imagination is gone from you forever. You've completed the game... but at a terrible price."
+        return game_complete
+
     else:
         print "I didn't recognize that command."
 
