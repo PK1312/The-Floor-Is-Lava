d66083bd073a1593252053ca1cbd13d97e6e38ba
Grant MacGillivray <grant@snuggle.cat>
2016-04-09 20:05:57 -0700

    Bug fix
    Accidently broke the game a little bit. Fixed it now, though.

--- floor_lava.py
+++ floor_lava.py
@@ -71,7 +71,7 @@ def get_input(x):
         print "You can't get ye flask. Now you need to sit around wondering why in the world you can't get ye flask."
     elif x.lower() == "look self" or x.lower() == "look at self" or x.lower() == "look at myself":
         print "You're wearing your pajamas. They have polar bears on them. Your mom has a matching pair. It's dorky as hell but also you love them."
-    elif x.lower() == "go bed" or x.lower() == "go to bed" or x.lower() == "get back into bed" or x.lower() == "get back in bed" or x.lower() == "get in bed" or "jump into bed":
+    elif x.lower() == "go bed" or x.lower() == "go to bed" or x.lower() == "get back into bed" or x.lower() == "get back in bed" or x.lower() == "get in bed" or x.lower() == "jump into bed":
         if player_location == "bed":
             print "You're already in bed. You're supposed to be getting OUT of bed, ya dang dingus."
         elif player_location == "towel":
