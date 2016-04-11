284431c2ed62e4b2ee7b8e9fb60a36863e78b994
Grant <gdawg3@gmail.com>
2016-04-10 19:02:30 -0700

    Update floor_lava.py
    apparently github desktop just, uh, straight up doesn't work, so here's the actual version with lots of bug fixes and junk.

--- floor_lava.py
+++ floor_lava.py
@@ -1,4 +1,4 @@
-#todo- locked closet door. Maybe monster in closet. Item under bed.
+#todo- Maybe monster in closet. Item under bed.
 
 name = raw_input("Please enter your name: ")
 print "Hello, " + name + ". You find yourself in your bedroom. The morning sun streams in through your blinds. It's 7 AM, and you need to get ready for school. It's the first day of 4th grade and you can't be late."
@@ -6,11 +6,13 @@ print "Hello, " + name + ". You find yourself in your bedroom. The morning sun s
 player_location = "bed"
 player_clothing = "pajamas"
 game_complete = False
+has_key = False
 
 def get_input(x):
     global player_location
     global player_clothing
     global game_complete
+    global has_key
     if x.lower() == "get up" or x.lower() == "stand up" or x.lower() == "get out of bed" or x.lower() == "wake up" or x.lower() == "roll out of bed":
         if player_location == "towel":
             print "You're as up as you can get."
@@ -18,7 +20,14 @@ def get_input(x):
             print "You cannot. The floor is lava."
 
     elif x.lower() == "look" or x.lower() == "look room" or x.lower() == "look floor" or x.lower() == "look lava" or x.lower() == "look bedroom" or x.lower() == "look around":
-        print "You look around your room. The floor glows redly, and you feel the heat rolling off it in waves. Floating conspiculously in the center of your room is a TOWEL. You can also see your CLOSET DOOR, behind which lies your school uniform. On the other side of the wall from your CLOSET DOOR is the EXIT."
+        print "You look around your room. The floor glows redly, and you feel the heat rolling off it in waves. Floating conspiculously in the center of your room is a TOWEL. You can also see your CLOSET DOOR, behind which lies your school uniform. On the other side of the wall from your CLOSET DOOR is the EXIT. Nex to your bed is the NIGHTSTAND, atop which you can see a SMALL BOX."
+
+    elif x.lower() == "look nightstand" or x.lower() == "look at nightstand" or x.lower() == "examine nightstand" or x.lower() == "look night stand" or x.lower() == "look at night stand" or x.lower() == "examine night stand" or x.lower() == "look box" or x.lower() == "look at box" or x.lower() == "look at small box" or x.lower() == "look small box":
+        print "It's a nightstand for your reading lamp. Atop the nightstand is a small box."
+    elif x.lower() == "get box" or x.lower() == "get small box" or x.lower() == "open box" or x.lower() == "open small box":
+        has_key = True
+        print "You open the small box. Inside is a small brass key, which you slip into your pocket."
+        return has_key
 
     elif x.lower() == "get on towel" or x.lower() == "go to towel" or x.lower() == "go towel" or x.lower() == "stand on towel" or x.lower() == "get onto towel" or x.lower() == "get on to towel" or x.lower() == "get on the towel" or x.lower() == "jump towel" or x.lower() == "jump onto the towel" or x.lower() == "jump onto towel" or x.lower() == "jump on to the towl":
         player_location = "towel"
@@ -27,14 +36,18 @@ def get_input(x):
 
     elif x.lower() == "open door" or x.lower() == "open" or x.lower() == "door":
         if player_location == "bed":
-            print "You stretch your little arms as far as you can, but there's just too much lava between you and the door."
+            print "You strain to reach the closet door from the foot of your bed, but your tiny arms are simply not long enough to reach. If only there was a way to get closer to the door..."
         elif player_location == "towel":
             print "Which door are you trying to open, your CLOSET DOOR or the EXIT DOOR?"
     elif x.lower() == "open closet" or x.lower() == "open closet door" or x.lower() == "closet door" or x.lower() == "closet":
-        player_clothing = "uniform"
-        print "You've done it! You manage to get the closet door open, and grab your SCHOOL UNIFORM. It's difficult on your floating towel, but you manage to change into it, letting your pajamas burn up in the lava."
-        return player_clothing
-
+        if player_location == "towel" and has_key == True:
+            player_clothing = "uniform"
+            print "You've done it! You manage to get the closet door open, and grab your SCHOOL UNIFORM. It's difficult on your floating towel, but you manage to change into it, letting your pajamas burn up in the lava."
+            return player_clothing
+        elif player_location == "bed":
+            print "You strain to reach the closet door from the foot of your bed, but your tiny arms are simply not long enough to reach. If only there was a way to get closer to the door..."
+        elif player_location == "towel" and has_key == False:
+            print "You jiggle the handle on your closet, but it's locked. You make mom lock it at night so the monster can't get you."
     elif x.lower() == "exit" or x.lower() == "open exit" or x.lower() == "open exit door" or x.lower() == "go exit" or x.lower() == "move exit" or x.lower() == "leave room" or x.lower() == "leave":
         if player_location == "towel" and player_clothing == "uniform":
             print "You open the door. You see your mother putting breakfast on the table. You sigh, and the floor cools back to carpet. Time for school. Congrats, " + name + "! You won the game! For now..."
@@ -46,7 +59,6 @@ def get_input(x):
             print "You have to get out of bed before you can leave."
         elif player_location == "bed" and player_clothing == "uniform":
             print "wait what, no, you shouldn't get back into bed after putting on your uniform what are you doing."
-
     elif x.lower() == "help":
         print "This is your standard test based adventure game! Type commands to see what works. If you're stuck, try using 'look'!"
 
@@ -65,18 +77,14 @@ def get_input(x):
         game_complete = True
         print "Your childlike innocence dies. The floor is just carpet. You get ready for school, but the spark of imagination is gone from you forever. You've completed the game... but at a terrible price."
         return game_complete
-    elif x.lower() == "dennis":
-        print "Nice try, Thy Dungeonman."
-    elif x.lower() == "get ye flask":
-        print "You can't get ye flask. Now you need to sit around wondering why in the world you can't get ye flask."
-    elif x.lower() == "look self" or x.lower() == "look at self" or x.lower() == "look at myself":
-        print "You're wearing your pajamas. They have polar bears on them. Your mom has a matching pair. It's dorky as hell but also you love them."
-    elif x.lower() == "go bed" or x.lower() == "go to bed" or x.lower() == "get back into bed" or x.lower() == "get back in bed" or x.lower() == "get in bed" or x.lower() == "jump into bed":
-        if player_location == "bed":
-            print "You're already in bed. You're supposed to be getting OUT of bed, ya dang dingus."
-        elif player_location == "towel":
-            player_location = "bed"
-            print "sigh. you get back into bed. it's now 7:15 and your mom is mad."
+    elif x.lower() == "eat":
+        print "You have nothing to eat. The smell of bacon wafts in from the kitchen, but you'll need to put on your uniform before you can get breakfast."
+    elif x.lower() == "brush my teeth" or x.lower() == "brush teeth":
+        print "Your toothbrush is in the bathroom. You cannot brush your teeth yet. Besides, you're not one of those weirdos who brushes their teeth before breakfast."
+    elif x.lower() == "shower" or x.lower() == "take shower" or x.lower() == "take a shower" or x.lower() == "take a bath" or x.lower() == "take bath" or x.lower() == "bath":
+        print "You're in your bedroom, you can't take a shower or bath here. Plus, the floor is lava, you have bigger issues on your mind."
+    elif x.lower() == "look at towel" or x.lower() == "look towel" or x.lower() == "examine towel":
+        print "A vertible island among this hellish sea that surrounds you, since it's still damp from the last time you used it."
 
     else:
         print "I didn't recognize that command."
